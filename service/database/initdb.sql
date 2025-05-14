27cac3eecfad223099d1195fc64d9422ec585f42
venditti.2031589@studenti.uniroma1.it <venditti.2031589@studenti.uniroma1.it>
2025-05-14 15:01:37 +0200

    initdb.sql attempt

---
+++ service/database/initdb.sql
@@ -0,0 +1,44 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS "users" (
+    id INTEGER PRIMARY KEY,
+    username TEXT NOT NULL UNIQUE,
+    photoId TEXT,
+    FOREIGN KEY (photoId) REFERENCES images(uuid)
+);
+
+CREATE TABLE IF NOT EXISTS "conversations" (
+    id INTEGER PRIMARY KEY,
+    isGroup BOOLEAN NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS "messages" (
+    id INTEGER PRIMARY KEY,
+    senderId INTEGER NOT NULL,
+    conversationId INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (senderId) REFERENCES users(id),
+    FOREIGN KEY (conversationId) REFERENCES conversations(id)
+)
+
+CREATE TABLE IF NOT EXISTS "reactions" (
+    id INTEGER PRIMARY KEY,
+    messageId INTEGER NOT NULL,
+    senderId INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (messageId) REFERENCES messages(id),
+    FOREIGN KEY (senderId) REFERENCES users(id)
+)
+
+CREATE TABLE IF NOT EXISTS "participants" (
+    userId INTEGER NOT NULL,
+    conversationId INTEGER NOT NULL,
+    FOREIGN KEY (userId) REFERENCES users(id),
+    FOREIGN KEY (conversationId) REFERENCES conversations(id) ON DELETE CASCADE,
+)
+
+CREATE TABLE IF NOT EXISTS "images" (
+    uuid TEXT PRIMARY KEY,
+)
+
