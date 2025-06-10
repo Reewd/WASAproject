15f5225f4b47e9b7a3f7ceab9e5db12a938f1aec
Reewd <venditti.2031589@studenti.uniroma1.it>
2025-06-10 17:08:27 +0200

    Fixes

--- service/database/db-participants.go
+++ service/database/db-participants.go
@@ -11,7 +11,7 @@ func (db *appdbimpl) InsertParticipants(conversationId int64, userId []int64) er
 	return nil
 }
 
-func (db *appdbimpl) RemoveParticipant(conversationId int64, userId string) error {
+func (db *appdbimpl) RemoveParticipant(conversationId int64, userId int64) error {
 	stmt := `DELETE FROM participants WHERE conversation_id = ? AND user_id = ?`
 	_, err := db.c.Exec(stmt, conversationId, userId)
 	if err != nil {
