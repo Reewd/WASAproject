1d243be6072bd5b9c98aaa5d2e897424d47294fa
Reewd <venditti.2031589@studenti.uniroma1.it>
2025-07-01 01:46:15 +0200

    Added InsertDelivered and InsertRead

--- service/database/db-status.go
+++ service/database/db-status.go
@@ -17,3 +17,33 @@ func (db *appdbimpl) InsertSent(messageId int64, conversationId int64, recipient
 	}
 	return nil
 }
+
+func (db *appdbimpl) InsertDelivered(conversationId int64, recipientId int64) error {
+	stmt := `
+        UPDATE message_status 
+        SET status = 'delivered' 
+        WHERE recipientId = ? AND status = 'sent'`
+
+	_, err := db.c.Exec(stmt, conversationId, recipientId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (db *appdbimpl) InsertRead(conversationId int64, recipientId int64) error {
+	stmt := `
+        UPDATE message_status 
+        SET status = 'read' 
+        WHERE conversationId = ? 
+          AND recipientId = ? 
+          AND status = 'delivered'`
+
+	_, err := db.c.Exec(stmt, conversationId, recipientId)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
