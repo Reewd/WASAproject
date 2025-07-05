8dd975b0d10a165565778fb8ca27921b1e05bccd
Reewd <venditti.2031589@studenti.uniroma1.it>
2025-07-05 16:11:50 +0200

    Refactor InsertSent to remove prepared statement and use direct execution

--- service/database/db-status.go
+++ service/database/db-status.go
@@ -3,14 +3,9 @@ package database
 func (db *appdbimpl) InsertSent(messageId int64, conversationId int64, recipientIds []int64) error {
 	status := "sent"
 	stmt := `INSERT INTO message_status (messageId, conversationId, recipientId, status) VALUES (?, ?, ?, ?)`
-	preparedStmt, err := db.c.Prepare(stmt)
-	if err != nil {
-		return err
-	}
-	defer preparedStmt.Close()
 
 	for _, recipientId := range recipientIds {
-		_, err := preparedStmt.Exec(messageId, conversationId, recipientId, status)
+		_, err := db.c.Exec(stmt, messageId, conversationId, recipientId, status)
 		if err != nil {
 			return err
 		}
